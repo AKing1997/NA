42b5d59922cd20c5f5be399d8436b1cf6ce077f5
Naiel <naiel.fdez@gmail.com>
2024-09-26 12:27:39 +0200

    implemented mint_real_world_asset_fungible & mint_real_world_asset_non_fungible

--- security_token/src/security_token.rs
+++ security_token/src/security_token.rs
@@ -9,13 +9,11 @@ pub struct IdentityData {
 
 #[derive(ScryptoSbor, NonFungibleData, Clone)]
 pub struct RealWorldAsset {
-    pub asset_id: String,       // Identificador único del activo
-    pub asset_type: String,     // Tipo de activo (por ejemplo, acción, bono, propiedad)
-    pub owner: String,          // Nombre del propietario o ID del propietario
-    pub value: Decimal,         // Valor del activo
-    pub metadata: HashMap<String, String>,  // Cualquier metadato adicional
+    pub asset_id: String,
+    pub name: String,
+    pub description: String,
+    pub value: Decimal,
 }
-
 
 #[blueprint]
 mod security_token {
@@ -34,8 +32,8 @@ mod security_token {
             create_identity_client => restrict_to: [agent, manager, admin];
             exchange_xrd_for_secure_token => restrict_to: [client];
             change_price_per_token => restrict_to: [manager, admin];
-            // mint_real_world_asset_fungible => restrict_to: [client];
-            // mint_real_world_asset_non_fungible => restrict_to: [client];
+            mint_real_world_asset_fungible => restrict_to: [client, admin];
+            mint_real_world_asset_non_fungible => restrict_to: [client, admin];
         }
     }
 
@@ -47,6 +45,7 @@ mod security_token {
         secure_token: Vault,
         price_per_token: Decimal,
         component_address: ComponentAddress,
+        asset_non_fungible_manager: ResourceManager,
     }
 
     impl SecurityToken {
@@ -71,35 +70,65 @@ mod security_token {
                 "name" => "Security Token", locked;
             }))
             .mint_roles(mint_roles! {
-                minter => rule!(require(owner_badge.resource_address()));
+                minter => rule!(require(global_caller(component_address)));
                 minter_updater => rule!(require(owner_badge.resource_address()));
             })
             .burn_roles(burn_roles! {
-                burner => rule!(require(owner_badge.resource_address()));
+                burner => rule!(require(global_caller(component_address)));
                 burner_updater => rule!(require(owner_badge.resource_address()));
             })
             .withdraw_roles(withdraw_roles! {
-                withdrawer => rule!(require(owner_badge.resource_address())); //client_badge_manager.address()) || 
+                withdrawer => rule!(require(client_badge_manager.address()) || require(owner_badge.resource_address()) || require(global_caller(component_address)));
                 withdrawer_updater => rule!(require(owner_badge.resource_address()));
             })
             .deposit_roles(deposit_roles! {
-                depositor => rule!(require(owner_badge.resource_address()));
+                depositor => rule!(require(client_badge_manager.address()) || require(owner_badge.resource_address()) || require(global_caller(component_address)));
                 depositor_updater => rule!(require(owner_badge.resource_address()));
             })
             .freeze_roles(freeze_roles! {
-                freezer => rule!(require(owner_badge.resource_address()));
+                freezer => rule!(require(global_caller(component_address)));
                 freezer_updater => rule!(require(owner_badge.resource_address()));
             })
             .recall_roles(recall_roles! {
-                recaller => rule!(require(owner_badge.resource_address()));
+                recaller => rule!(require(global_caller(component_address)));
                 recaller_updater => rule!(require(owner_badge.resource_address()));
             })
             .divisibility(DIVISIBILITY_MAXIMUM)
             .mint_initial_supply(1000000)
             .into();
 
-
             let owner_proof = owner_badge.as_fungible().create_proof_of_amount(1);
+
+            let asset_non_fungible_manager: ResourceManager = ResourceBuilder::new_string_non_fungible::<RealWorldAsset>(OwnerRole::None)
+            .metadata(metadata!(init {
+                "name" => "Real World Asset", locked;
+            }))
+            .mint_roles(mint_roles! {
+                minter => rule!(require(global_caller(component_address)));
+                minter_updater => rule!(require(owner_badge.resource_address()));
+            })
+            .burn_roles(burn_roles! {
+                burner => rule!(require(global_caller(component_address)));
+                burner_updater => rule!(require(owner_badge.resource_address()));
+            })
+            .withdraw_roles(withdraw_roles! {
+                withdrawer => rule!(require(client_badge_manager.address()) || require(owner_badge.resource_address()) || require(global_caller(component_address)));
+                withdrawer_updater => rule!(require(owner_badge.resource_address()));
+            })
+            .deposit_roles(deposit_roles! {
+                depositor => rule!(require(client_badge_manager.address()) || require(owner_badge.resource_address()) || require(global_caller(component_address)));
+                depositor_updater => rule!(require(owner_badge.resource_address()));
+            })
+            .freeze_roles(freeze_roles! {
+                freezer => rule!(require(global_caller(component_address)));
+                freezer_updater => rule!(require(owner_badge.resource_address()));
+            })
+            .recall_roles(recall_roles! {
+                recaller => rule!(require(global_caller(component_address)));
+                recaller_updater => rule!(require(owner_badge.resource_address()));
+            })
+            .create_with_no_initial_supply();
+    
             LocalAuthZone::push(owner_proof);
 
             let identity_instance: SecurityToken = Self {
@@ -109,7 +138,8 @@ mod security_token {
                 xrd_vault,
                 secure_token: Vault::with_bucket(secure_token),
                 price_per_token,
-                component_address
+                component_address,
+                asset_non_fungible_manager
             };
 
             let global_identity: Global<SecurityToken> = identity_instance
@@ -134,6 +164,67 @@ mod security_token {
             addresses.insert("Client Badge Address".to_string(), self.client_badge_manager.address());
             addresses.insert("XRD Vault Address".to_string(), self.xrd_vault.resource_address());
             addresses
+        }
+        
+        pub fn mint_real_world_asset_fungible(
+            &self,
+            name: String,
+            assert_id: String,
+            description: String,
+            initial_supply: Decimal,
+            divisibility: u8
+        ) -> Bucket {
+            ResourceBuilder::new_fungible(OwnerRole::None)
+            .metadata(metadata!(init {
+                "name" => name.clone(), updatable;
+                "assert_id" => assert_id.clone(), updatable;
+                "description" => description.clone(), updatable;
+            }))
+            .mint_roles(mint_roles! {
+                minter => rule!(require(global_caller(self.component_address)));
+                minter_updater => rule!(require(global_caller(self.component_address)));
+            })
+            .burn_roles(burn_roles! {
+                burner => rule!(require(global_caller(self.component_address)));
+                burner_updater => rule!(require(global_caller(self.component_address)));
+            })
+            .withdraw_roles(withdraw_roles! {
+                withdrawer => rule!(require(self.client_badge_manager.address()) || require(global_caller(self.component_address)));
+                withdrawer_updater => rule!(require(global_caller(self.component_address)));
+            })
+            .deposit_roles(deposit_roles! {
+                depositor => rule!(require(self.client_badge_manager.address()) || require(global_caller(self.component_address)));
+                depositor_updater => rule!(require(global_caller(self.component_address)));
+            })
+            .freeze_roles(freeze_roles! {
+                freezer => rule!(require(global_caller(self.component_address)));
+                freezer_updater => rule!(require(global_caller(self.component_address)));
+            })
+            .recall_roles(recall_roles! {
+                recaller => rule!(require(global_caller(self.component_address)));
+                recaller_updater => rule!(require(global_caller(self.component_address)));
+            })
+            .divisibility(divisibility)
+            .mint_initial_supply(initial_supply)
+            .into()
+        }
+
+        pub fn mint_real_world_asset_non_fungible(
+            &mut self,
+            asset_id: String,
+            name: String,
+            description: String,
+            value: Decimal
+        ) -> Bucket {
+            let real_world_asset = RealWorldAsset {
+                asset_id: asset_id.clone(),
+                name,
+                description,
+                value
+            };
+            let non_fungible_id = NonFungibleLocalId::string(asset_id.clone()).expect("Failed to create NonFungibleLocalId");
+
+            self.asset_non_fungible_manager.mint_non_fungible(&non_fungible_id, real_world_asset.clone())
         }
 
         pub fn exchange_xrd_for_secure_token(&mut self, amount_of_xrd: Bucket) -> Bucket {
